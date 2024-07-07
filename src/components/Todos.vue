4279dce9200c237cf259e42b7cc60f1b79cefe61
john naduer <naduer789@gmail.com>
2024-07-07 01:51:23 -0500

    feat: add new form

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,15 +1,19 @@
 <template>
   <div>
-    <h1>Hola Mundo</h1>
+    <form action="">
+      <label for="name">Ingresar Nombre:</label>
+      <input type="text" v-model="name">
+      <button type="submit">enviar</button>
+    </form>
   </div>
 </template>
 
 <script setup lang="ts">
+import {ref} from 'vue';
+
+const name = ref('');
 
 </script>
 
 <style scoped>
-
-
-
 </style>
